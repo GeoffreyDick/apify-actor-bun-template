546da2d8b4ec0e184ab54b2024147ad974bc7cee
GeoffreyDick <git@geoffdick.com>
2023-09-03 15:14:41 -0600

    feat: add Apify

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+import { Actor, log, LogLevel } from 'apify'
+
+// Initialize the Apify Actor
+await Actor.init()
+
+// Set the log level
+log.setLevel(LogLevel.DEBUG)
+
+// Crawl stuff
+
+// Exit the Apify Actor
+await Actor.exit()
